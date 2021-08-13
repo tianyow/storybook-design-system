49e082b6f633e2a9d87bd2b3486ce4d3229e5a4a
TY <tyyang@pershing.com.tw>
2021-08-13 16:23:31 +0800

    make Button pop

--- src/Button.js
+++ src/Button.js
@@ -33,10 +33,11 @@ const SIZES = {
 };
 
 const StyledButton = styled.button`
-  border: 0;
+  border: 10px solid red;
   border-radius: 3em;
   cursor: pointer;
   display: inline-block;
+  font-size: 20px;
   overflow: hidden;
   padding: ${props => (props.size === SIZES.SMALL ? '8px 16px' : '13px 20px')};
   position: relative;
